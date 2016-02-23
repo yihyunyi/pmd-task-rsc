51b1a597d989bd9ed44c80395992bb4bcdc40428
yihyunyi <yihyunyi@bluedigm.com>
2016-02-23 04:52:51 +0000

    fix

--- pipe-task/pmd-exe.sh
+++ pipe-task/pmd-exe.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-pmd_report_file='./pmd.xml'
-pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml rsc-git/pmd-rules/EgovInspectionRules_kor.xml"
+pmd_report_file='pmd.xml'
+pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml rsc-git/pmd-rules/EgovInspectionRules_kor.xml > $pmd_report_file"
 pmd_err_word='<violation'
 pmd_report_cnt="$(grep -c $pmd_err_word $pmd_report_file)"
 
-java $pmd_cmd > $pmd_report_file
+java $pmd_cmd
 
 if [ $? -eq 0 ]; then
 	cnt=$pmd_report_cnt
