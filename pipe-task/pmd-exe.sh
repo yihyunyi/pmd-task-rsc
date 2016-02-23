f8c155ffcd19423ef1515b9b5832064a8ef6dea5
yihyunyi <yihyunyi@bluedigm.com>
2016-02-23 06:49:53 +0000

    pmd-exe.sh colour

--- pipe-task/pmd-exe.sh
+++ pipe-task/pmd-exe.sh
@@ -1,23 +1,16 @@
 #!/bin/sh
 
-pmd_report_file='rsc-git/pmd.xml'
 pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml rsc-git/pmd-rules/EgovInspectionRules_kor.xml"
 pmd_err_word='<violation'
 pmd_report_cnt="$(java $pmd_cmd | grep -c $pmd_err_word)"
 
-#java $pmd_cmd > $pmd_report_file
+cnt=$pmd_report_cnt
 
-#if [ $? -eq 0 ]; then
-	cnt=$pmd_report_cnt
-
-        if [ $cnt -gt 0 ]; then
-                echo "pmd violation $cnt deteded"
-		java $pmd_cmd
-                exit 1
-        else
-                echo "pmd check success"
-                exit 0
-        fi
-#else
-        exit $?
-#fi
+if [ $cnt -gt 0 ]; then
+	echo -e "\033[40;37m pmd violation $cnt deteded\033[0m"
+	java $pmd_cmd
+        exit 1
+else
+        echo "pmd check success"
+        exit 0
+fi
