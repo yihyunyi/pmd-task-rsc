671d80bb37f285b89517ddab30c17ba217d36e25
yihyunyi <yihyunyi@bluedigm.com>
2016-02-23 05:02:56 +0000

    test

--- pipe-task/pmd-exe.sh
+++ pipe-task/pmd-exe.sh
@@ -3,11 +3,11 @@
 pmd_report_file='rsc-git/pmd.xml'
 pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml rsc-git/pmd-rules/EgovInspectionRules_kor.xml"
 pmd_err_word='<violation'
-pmd_report_cnt="$(grep -c $pmd_err_word $pmd_report_file)"
+pmd_report_cnt="$(java $pmd_cmd | grep -c $pmd_err_word)"
 
-java $pmd_cmd > $pmd_report_file
+#java $pmd_cmd > $pmd_report_file
 
-if [ $? -eq 0 ]; then
+#if [ $? -eq 0 ]; then
 	cnt=$pmd_report_cnt
 
         if [ $cnt -gt 0 ]; then
@@ -17,6 +17,6 @@ if [ $? -eq 0 ]; then
                 echo "pmd check success"
                 exit 0
         fi
-else
+#else
         exit $?
-fi
+#fi
