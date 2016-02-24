05ac86f06e7eff5b58771b4666e3e7c51e72d00a
yihyunyi <yihyunyi@bluedigm.com>
2016-02-24 02:29:17 +0000

    tee test

--- pipe-task/pmd-exe.sh
+++ pipe-task/pmd-exe.sh
@@ -2,13 +2,13 @@
 
 pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml rsc-git/pmd-rules/EgovInspectionRules_kor.xml"
 pmd_err_word='<violation'
-pmd_report_cnt="$(java $pmd_cmd | grep -c $pmd_err_word)"
+pmd_report_file='pmd_report.xml'
 
-cnt=$pmd_report_cnt
+java $pmd_cmd | tee $pmd_report_file
+cnt="$(grep -c $pmd_err_word $pmd_report_file)"
 
 if [ $cnt -gt 0 ]; then
 	echo "pmd violation $cnt deteded"
-	java $pmd_cmd
         exit 1
 else
         echo "pmd check success"
