6549ae881818e9ba48ac8d1f6f8af6f827192cdb
yihyunyi <yihyunyi@bluedigm.com>
2016-02-25 00:16:06 +0000

    modifiy path

--- pipe-task/pmd-exe.sh
+++ pipe-task/pmd-exe.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml rsc-git/pmd-rules/EgovInspectionRules_kor.xml"
+pmd_cmd="-Xmx512m -jar /pmd-bin-4.2.6/lib/pmd-4.2.6.jar ./ xml pmd-rsc-git/pmd-rules/EgovInspectionRules_kor.xml"
 pmd_err_word='<violation'
 pmd_report_file='pmd_report.xml'
 
